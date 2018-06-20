1ca02152104e7ea2cca9dd2d92d0f1e98785d5e8
Nu Deployer (10.129.216.106-black-agent-all) <tech@nubank.com.br>
2018-06-20 19:35:15 +0000

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject state-flow "0.1.0"
+(defproject state-flow "0.1.1-SNAPSHOT"
   :description "Postman-like integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
   :license {:name "Proprietary"}
