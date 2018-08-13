467f162d00961f6e9bc1d8b8a42e18f4b51a96b3
ericvm <ericvm@users.noreply.github.com>
2018-08-13 18:19:54 -0300

    Request map (#3)
    * request helper using request-map
    
    * deprecated note
    
    * some fixes on expected responses from req
    
    * fix version

--- src/state_flow/helpers/http.clj
+++ src/state_flow/helpers/http.clj
@@ -3,7 +3,8 @@
             [nu.monads.state :as state]
             [state-flow.helpers.core :as helpers]))
 
-(defn make-request
+(defn ^:deprecated make-request
+  "Use helpers.servlet/request instead"
   [req-fn]
   (assert (fn? req-fn) "First argument must be a function")
   (state/wrap-fn req-fn))
--- src/state_flow/helpers/servlet.clj
+++ src/state_flow/helpers/servlet.clj
@@ -1,10 +1,11 @@
 (ns state-flow.helpers.servlet
   (:require [common-io.interceptors.wire :as wire]
             [common-io.test-helpers :as th]
+            [state-flow.helpers.core :as helpers]
             [common-test.http :as http]))
 
-(defn req
-  "Experimental"
+(defn ^:private req
+  "Experimental rewrite of common-test.http/req! with removed assertions"
   [servlet method content-type uri body]
   (th/with-debug-error-logging
     (let [request-content-type (get th/content-headers content-type)]
@@ -13,4 +14,14 @@
             {:keys [headers status body] :as response-map} (http/raw-req! servlet method uri (http/parse-data body content-type) request-headers)
             accept                        (get request-headers "Accept" request-content-type)
             response-type                 (wire/header->content-type (get headers "Content-Type" accept))]
-        (merge response-map {:body (th/output-stream->data body response-type)})))))
+        (merge response-map
+               (when-not (clojure.string/blank? body)
+                 {:body (th/output-stream->data body response-type)}))))))
+
+(defn request
+  [{:keys [method url content-type body scopes]
+    :or   {content-type :edn
+           body         {}
+           scopes       ""}}]
+  (helpers/with-servlet
+    #(http/with-scopes scopes (req % method content-type url body))))
