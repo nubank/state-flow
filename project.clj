1518bd5dcc3e7446828f7276479c140164ad09b9
Nu Deployer (10.129.215.40-blue-agent-release-lib) <tech@nubank.com.br>
2018-08-14 18:48:43 +0000

    Version 1.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject state-flow "1.3.1-SNAPSHOT"
+(defproject state-flow "1.3.1"
   :description "Postman-like integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
   :license {:name "Proprietary"}
