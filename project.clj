ecb8174afb4f373c35184149d38488c156e27e7b
nu-release-lib <tech+nu-release-lib@nubank.com.br>
2018-12-18 19:09:37 +0000

    Version 1.7.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject state-flow "1.7.5"
+(defproject state-flow "1.7.6-SNAPSHOT"
 
   :description "Postman-like integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
