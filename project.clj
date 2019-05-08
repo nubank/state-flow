d341722abaddd60df07dcea82571d83a1ac0374c
Eric Velten <eric.velten@nubank.com.br>
2019-05-08 15:05:29 -0300

    fix matcher-combinators dependencies

--- project.clj
+++ project.clj
@@ -14,14 +14,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [funcool/cats "2.3.2"]]
+                 [funcool/cats "2.3.2"]
+                 [nubank/matcher-combinators "0.9.0"]]
 
   :exclusions   [log4j]
 
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["config"]
                    :dependencies [[ns-tracker "0.3.1"]
-                                  [nubank/matcher-combinators "0.3.4"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [midje "1.9.1"]
                                   [org.clojure/java.classpath "0.3.0"]]}}
