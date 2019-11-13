b6058f36b4fdf373ccd667d1fea4eda03a7e0636
sovelten <7022879+sovelten@users.noreply.github.com>
2019-11-13 16:23:27 -0300

    Update dependencies (#40)
    * Update dependencies
    
    * changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## [2.0.2]
+- Update cats and matcher-combinators to latest versions
+
 ## [2.0.1]
 - Allow `(str ...)` to be a valid flow description
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/state-flow "2.0.1"
+(defproject nubank/state-flow "2.0.2"
   :description "Postman-like integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
   :license {:name "MIT"}
@@ -13,8 +13,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.4.0"]
-                 [funcool/cats "2.3.2"]
-                 [nubank/matcher-combinators "1.2.1"]]
+                 [funcool/cats "2.3.3"]
+                 [nubank/matcher-combinators "1.2.4"]]
 
   :exclusions   [log4j]
 
