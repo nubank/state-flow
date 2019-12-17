249752cdf6cbe86a6f4b75065e066bed63258973
Phillip Mates <phillip.mates@nubank.com.br>
2019-12-17 20:49:24 +0100

    add nsorg and cljfmt linters + bump matcher-combinators

--- project.clj
+++ project.clj
@@ -8,15 +8,20 @@
             [lein-vanity "0.2.0"]
             [s3-wagon-private "1.3.1"]
             [lein-ancient "0.6.15"]
+            [lein-cljfmt "0.6.1"]
+            [lein-nsorg "0.2.0"]
             [changelog-check "0.1.0"]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.4.0"]
                  [funcool/cats "2.3.3"]
-                 [nubank/matcher-combinators "1.2.4"]]
+                 [nubank/matcher-combinators "1.2.6"]]
 
   :exclusions   [log4j]
+
+  :cljfmt {:indents {mlet [[:block 1]]
+                     flow [[:block 1]]}}
 
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["config"]
@@ -26,6 +31,8 @@
                                   [org.clojure/java.classpath "0.3.0"]]}}
 
   :aliases {"coverage" ["cloverage" "-s" "coverage"]
+            "lint"     ["do" ["cljfmt" "check"] ["nsorg"]]
+            "lint-fix" ["do" ["cljfmt" "fix"] ["nsorg" "--replace"]]
             "loc"      ["vanity"]}
 
   :repl-options  {:init-ns user}
