b79ba5f71a4cf6678226d0388f199d69fe97a0ba
Sophia Velten de Melo <sophia.velten@nubank.com.br>
2019-12-20 15:15:24 -0300

    fix some testsc

--- test/state_flow/core_test.clj
+++ test/state_flow/core_test.clj
@@ -1,6 +1,7 @@
 (ns state-flow.core-test
   (:require [cats.core :as m]
             [cats.monad.state :as state]
+            [matcher-combinators.midje :refer [match]]
             [midje.sweet :refer :all]
             [state-flow.core :as state-flow]
             [state-flow.midje :as midje]
@@ -43,19 +44,19 @@
 
 (facts "on run flow"
   (fact "run flow of single step"
-    (state/exec (state-flow/flow "single step" increment-two-value) {:value 0}) => {:meta {:description [["single step"]
-                                                                                                         []]}
+    (state/exec (state-flow/flow "single step" increment-two-value) {:value 0}) => {:meta  {:description [["single step"]
+                                                                                                          []]}
                                                                                     :value 2})
   (fact "flow with two steps"
     (second (state-flow/run (state-flow/flow "two step flow"
                               (state-flow/flow "first step" increment-two-value)
                               (state-flow/flow "second step" increment-two-value))
-              {:value 0})) => {:meta {:description [["two step flow"]
-                                                    ["two step flow" "first step"]
-                                                    ["two step flow"]
-                                                    ["two step flow" "second step"]
-                                                    ["two step flow"]
-                                                    []]} :value 4})
+              {:value 0})) => {:meta                                         {:description [["two step flow"]
+                                                                                            ["two step flow" "first step"]
+                                                                                            ["two step flow"]
+                                                                                            ["two step flow" "second step"]
+                                                                                            ["two step flow"]
+                                                                                            []]} :value 4})
 
   (fact "empty flow"
     (state-flow/run empty-flow {}) => irrelevant)
@@ -82,10 +83,15 @@
   (fact "nested-flow-with exception, returns exception and state before exception"
     (let [[left right] (state-flow/run bogus-flow {:value 0})]
       @left => (throws Exception "My exception")
-      right => {:meta {:description ["root" "child2"]}, :value 2}))
+      right => {:meta  {:description [["root"]
+                                      ["root" "child1"]
+                                      ["root"]
+                                      ["root" "child2"]]}
+                :value 2}))
 
   (fact "flow allows do-let style binding"
-    (second (state-flow/run flow-with-bindings {:value 2})) => {:meta {:description []} :value 4})
+    (second (state-flow/run flow-with-bindings {:value 2}))
+    => (match {:value 4}))
 
   (fact "run! throws exception"
     (state-flow/run! bogus-flow {:value 0}) => (throws Exception)))
@@ -94,8 +100,7 @@
 
   (fact "flow with initializer"
     (second (state-flow/run* {:init (constantly {:value 0})} nested-flow))
-    => {:meta  {:description []}
-        :value 4})
+    => (match {:value 4}))
 
   (fact "flow with cleanup"
     (-> (state-flow/run* {:init    (constantly {:value 0
@@ -107,13 +112,12 @@
         deref)
     => 0)
 
-  (fact "flow with initializer"
-    (state-flow/run* {:init   (constantly {:value 0})
-                      :runner (fn [flow state]
-                                [nil (state/exec flow state)])}
-                     nested-flow)
-    => [nil {:meta  {:description []}
-             :value 4}]))
+  (fact "flow with custom runner"
+    (second (state-flow/run* {:init   (constantly {:value 0})
+                             :runner (fn [flow state]
+                                       [nil (state/exec flow state)])}
+             nested-flow))
+    => (match {:value 4})))
 
 (facts "on as-step-fn"
   (let [increment-two-step (state-flow/as-step-fn (state/swap #(+ 2 %)))]
