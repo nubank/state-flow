13bd0c76a1a7a78d67a76bdaa717aa892c3a2f58
David Chelimsky <dchelimsky@gmail.com>
2019-12-26 15:09:10 -0600

    convert state-test to use clojure.test

--- test/state_flow/state_test.clj
+++ test/state_flow/state_test.clj
@@ -1,38 +1,46 @@
 (ns state-flow.state-test
-  (:require [cats.context :as ctx]
+  (:require [clojure.test :as t :refer [deftest testing is]]
             [cats.core :as m]
             [cats.data :as d]
             [cats.monad.exception :as e]
             [cats.monad.state :as state]
-            [cats.protocols :as p]
-            [midje.sweet :refer :all]
+            [midje.sweet :as midje]
             [state-flow.state :as sf.state]))
 
-(def postincrement
-  (m/mlet [x (sf.state/get)
-           _ (sf.state/put (+ x 1))]
-    (m/return x)))
+(deftest test-state
+  (let [increment-state       (m/mlet [x (sf.state/get)
+                                       _ (sf.state/put (inc x))]
+                                      (m/return x))
+        double-state          (sf.state/modify #(* 2 %))
+        state-with-fact-check (m/mlet [x increment-state
+                                       y increment-state
+                                       z (sf.state/get)]
+                                      (m/return (midje/fact "z is equal to initial state incremented by two"
+                                                            z => (+ x 2))))
+        state-with-assertion  (m/mlet [x increment-state
+                                       y increment-state
+                                       z (sf.state/get)]
+                                      (m/return (is (= z (+ x 2)))))]
+    (testing "modify state with get and put"
+      (is (= (d/pair 2 3)
+             (state/run increment-state 2))))
+    (testing "modify state with modify"
+      (is (= (d/pair 2 4)
+             (state/run double-state 2))))
+    (testing "state with fact check"
+      (is (= (d/pair true 2)
+             (state/run state-with-fact-check 0))))
+    (testing "state with clojure test assertion"
+      (is (= (d/pair true 2)
+             (state/run state-with-assertion 0))))
+    (testing "state with an exception"
+      (let [[res state] (state/run (m/>> double-state
+                                         double-state
+                                         (sf.state/modify (fn [s] (throw (Exception. "My exception"))))
+                                         double-state) 2)]
+        (is (e/failure? res))
+        (is (= 8 state))))))
 
-(def double-state (sf.state/modify #(* 2 %)))
-
-(fact "postincrement"
-  (state/run postincrement 1) => (d/pair 1 2))
-
-(def state-with-fact-check
-  (m/mlet [x postincrement
-           y postincrement
-           z (sf.state/get)]
-    (m/return (fact "z is equal to initial state incremented by two"
-                z => (+ x 2)))))
-
-(state/run state-with-fact-check 0)
-
-(def will-fail
-  (m/>> double-state
-        double-state
-        (sf.state/modify (fn [s] (throw (Exception. "My exception"))))
-        double-state))
-
-(fact "Error short-circuits execution"
-  (state/run will-fail 2) => (fn [[l r]] (and (e/failure? l)
-                                              (= r 8))))
+(comment
+  )
+(t/run-tests)
