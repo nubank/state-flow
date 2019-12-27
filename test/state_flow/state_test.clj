89ec93e296e2da41674b2ebfc033cfd48f44403f
David Chelimsky <dchelimsky@gmail.com>
2019-12-26 20:55:47 -0600

    remove duplicate tests

--- test/state_flow/state_test.clj
+++ test/state_flow/state_test.clj
@@ -4,35 +4,19 @@
             [cats.data :as d]
             [cats.monad.exception :as e]
             [cats.monad.state :as state]
-            [midje.sweet :as midje]
             [state-flow.state :as sf.state]))
 
 (deftest test-state
   (let [increment-state       (m/mlet [x (sf.state/get)
                                        _ (sf.state/put (inc x))]
                                       (m/return x))
-        double-state          (sf.state/modify #(* 2 %))
-        state-with-fact-check (m/mlet [x increment-state
-                                       y increment-state
-                                       z (sf.state/get)]
-                                      (m/return (midje/fact "z is equal to initial state incremented by two"
-                                                            z => (+ x 2))))
-        state-with-assertion  (m/mlet [x increment-state
-                                       y increment-state
-                                       z (sf.state/get)]
-                                      (m/return (is (= z (+ x 2)))))]
+        double-state          (sf.state/modify #(* 2 %))]
     (testing "modify state with get and put"
       (is (= (d/pair 2 3)
              (state/run increment-state 2))))
     (testing "modify state with modify"
       (is (= (d/pair 2 4)
              (state/run double-state 2))))
-    (testing "state with fact check"
-      (is (= (d/pair true 2)
-             (state/run state-with-fact-check 0))))
-    (testing "state with clojure test assertion"
-      (is (= (d/pair true 2)
-             (state/run state-with-assertion 0))))
     (testing "state with an exception"
       (let [[res state] (state/run (m/>> double-state
                                          double-state
