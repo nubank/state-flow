9f68731c16d707ee1c98b5d209eda383696f08c5
David Chelimsky <dchelimsky@gmail.com>
2020-01-29 09:51:35 -0600

    refactor match?

--- src/state_flow/probe.clj
+++ src/state_flow/probe.clj
@@ -12,20 +12,20 @@
 (defn ^:private retry
   "Tries at most n times, returns a vector with true and first element that succeeded
   or false and result of the first try"
-  [times pred? lazy-seq]
-  (let [remaining (drop-while (complement pred?) (take times lazy-seq))]
+  [times-to-try check-fn tries]
+  (let [remaining (drop-while (complement check-fn) (take times-to-try tries))]
     (if (empty? remaining)
-      [false (first lazy-seq)]
+      [false (first tries)]
       [true  (first remaining)])))
 
 (defn probe
   "evaluates state repeatedly with check-fn until check-fn succeeds or we try too many times"
   ([state check-fn]
-   (probe state check-fn {:sleep-time default-sleep-time :times-to-try default-times-to-try}))
+   (probe state check-fn {}))
   ([state check-fn {:keys [sleep-time times-to-try]
                     :or   {sleep-time   default-sleep-time
                            times-to-try default-times-to-try}}]
    (m/mlet [world (state/get)
-            :let [runs   (repeatedly #(do (Thread/sleep sleep-time) (state/eval state world)))
-                  result (retry times-to-try check-fn runs)]]
+            :let [tries  (repeatedly #(do (Thread/sleep sleep-time) (state/eval state world)))
+                  result (retry times-to-try check-fn tries)]]
      (state/return result))))
