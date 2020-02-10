bd83490acaa9eed46e6f4611c88539e2fc401dcf
Sophia Velten de Melo <sophia.velten@nubank.com.br>
2020-02-10 14:36:33 -0300

    change flow to step

--- src/state_flow/probe.clj
+++ src/state_flow/probe.clj
@@ -11,38 +11,38 @@
 
 (defn ^:private check
   "Applies check-fn to the return value of a step, returns the check result and the original value in a map"
-  [flow check-fn]
-  (m/mlet [value flow]
+  [step check-fn]
+  (m/mlet [value step]
     (state/return {:check-result (check-fn value)
                    :value        value})))
 
 (defn ^:private with-delay
   "Adds a delay before the step is run"
-  [flow delay]
-  (m/>> (state/wrap-fn #(Thread/sleep delay)) flow))
+  [step delay]
+  (m/>> (state/wrap-fn #(Thread/sleep delay)) step))
 
 (defn ^:private sequence-while*
-  "Like cats.core/sequence but with short circuiting when pred is satisfied by the return value of a flow"
-  [pred acc flows]
-  (if (empty? flows)
+  "Like cats.core/sequence but with short circuiting when pred is satisfied by the return value of a step"
+  [pred acc steps]
+  (if (empty? steps)
     acc
-    (m/mlet [result (first flows)
+    (m/mlet [result (first steps)
              results acc]
       (if (pred result)
         (state/return (conj results result))
-        (sequence-while* pred (state/return (conj results result)) (rest flows))))))
+        (sequence-while* pred (state/return (conj results result)) (rest steps))))))
 
 (defn ^:private sequence-while
-  [pred flows]
-  (sequence-while* pred (state/return []) flows))
+  [pred steps]
+  (sequence-while* pred (state/return []) steps))
 
 (defn probe
   "Internal use only. Evaluates step repeatedly with check-fn until check-fn succeeds or we try too many times"
-  ([flow check-fn]
-   (probe flow check-fn {}))
-  ([flow check-fn {:keys [sleep-time times-to-try]
+  ([step check-fn]
+   (probe step check-fn {}))
+  ([step check-fn {:keys [sleep-time times-to-try]
                    :or   {sleep-time   default-sleep-time
                           times-to-try default-times-to-try}}]
    (sequence-while :check-result
-                   (cons (check flow check-fn)
-                         (repeat (dec times-to-try) (with-delay (check flow check-fn) sleep-time))))))
+                   (cons (check step check-fn)
+                         (repeat (dec times-to-try) (with-delay (check step check-fn) sleep-time))))))
