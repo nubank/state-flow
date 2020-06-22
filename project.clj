9d2f18650cba2a5d6a39547abb97b7e9d0c53fb5
David Chelimsky <dchelimsky@gmail.com>
2020-06-22 09:31:26 -0500

    upgrade to matcher-combinators-2.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/state-flow "5.2.0"
+(defproject nubank/state-flow "5.3.0"
   :description "Integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
   :license {:name "MIT"}
