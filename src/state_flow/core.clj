8494af581836d0b0f17067d9701b471e39f9a0f8
Sophia Velten de Melo <sophia.velten@nubank.com.br>
2020-12-15 14:33:53 -0300

    remove opt

--- src/state_flow/core.clj
+++ src/state_flow/core.clj
@@ -273,8 +273,7 @@
            before-flow-hook       identity
            on-error               (comp throw-error!
                                         log-error
-                                        (filter-stack-trace default-stack-trace-exclusions))
-           assert-with-clojure-test? false}}
+                                        (filter-stack-trace default-stack-trace-exclusions))}}
    flow]
   (let [init-state+meta (vary-meta (init)
                                    assoc
