9746360961d612890295613e3c6e61db5f6f69cf
Phillip Mates <phillip.mates@nubank.com.br>
2021-07-29 16:25:21 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/state-flow "5.13.1"
+(defproject nubank/state-flow "5.13.2"
   :description "Integration testing with composable flows"
   :url "https://github.com/nubank/state-flow"
   :license {:name "MIT"}
